cf91234578ebdfcfb93eb7e49c8b062e6b6c016d
clstr021217-cpu <clstr021217@gmail.com>
2026-07-23 20:21:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-import os
-
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+from config import TELEGRAM_BOT_TOKEN
+from database import add_user, get_credit
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+
+    add_user(
+        user.id,
+        user.username if user.username else user.first_name
+    )
+
+    credit = get_credit(user.id)
+
     await update.message.reply_text(
-        "🤖 DMPT AI BOT\n\n"
-        "Bot berhasil online!\n"
-        "Versi 1.0"
+        f"""🤖 DMPT AI BOT
+
+Halo {user.first_name} 👋
+
+💳 Kredit : {credit}
+
+Bot berhasil online."""
     )
 
 def main():
-    app = Application.builder().token(TOKEN).build()
+    app = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
 
-    print("Bot berjalan...")
+    print("DMPT BOT ONLINE")
+
     app.run_polling()
 
 if __name__ == "__main__":
